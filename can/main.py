f52754fc1cebbac101629bb78d40e0ebc0878b57
Mahesh Pranav Chandrasekhar <mahesh@Maheshs-MacBook-Pro.local>
2024-05-03 19:11:11 -0400

    cleanup

--- can/main.py
+++ can/main.py
@@ -10,7 +10,7 @@
 #     os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")   #bring up can0 interface at 500kbps
 #     time.sleep(0.05)
 #     try:
-# 	    CAN1 = can.interface.Bus(channel='can0', bustype='socketcan')   #instantiate CAN object
+# 	    CAN0 = can.interface.Bus(channel='can0', bustype='socketcan')   #instantiate CAN object
 #     except OSError:
 # 	    print('Cannot find PiCAN board.')
 # 	    exit()
@@ -18,14 +18,14 @@
 
 #     #create listener and notifier             
 #     #(TODO: configure CAN connection (masks, etc))
-#     CAN1_listener = RECEIVE_MESSAGE                                        
-#     CAN1_notifier = can.Notifier(CAN1, [CAN1_listener])   #assign listener to notifier
+#     CAN0_listener = RECEIVE_MESSAGE                                        
+#     CAN0_notifier = can.Notifier(CAN0, [CAN0_listener])   #assign listener to notifier
 
 #     #send and receive messages
 #     # 1. send
 #     msg_data = [0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0, 0xF0]
 #     msg = can.Message(arbitration_id = 0x57, data = msg_data)
-#     CAN1.send(msg)
+#     CAN0.send(msg)
 #     os.system("sudo ip link set can0 down")
 
 # if __name__ == "__main__":
@@ -41,27 +41,27 @@ class MessageListener(can.Listener):
         print(f'Received message: {msg}')
 
 def main():
-    os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")  # Bring up can0 interface at 500kbps
-    os.system("sudo /sbin/ip link set can1 up type can bitrate 500000")
+    os.system("sudo ip link set can0 up type can bitrate 500000")  # Bring up can0 interface at 500kbps
+    os.system("sudo ip link set can1 up type can bitrate 500000")
     time.sleep(1)
     try:
-        CAN1 = can.interface.Bus(channel='can0', bustype='socketcan')  # Instantiate CAN object
+        CAN0 = can.interface.Bus(channel='can0', bustype='socketcan')  # Instantiate CAN object
     except OSError:
         print('Cannot find PiCAN board.')
         exit()
     print('Ready')
 
-    CAN1_listener = MessageListener()
-    CAN1_notifier = can.Notifier(CAN1, [CAN1_listener])  # Assign listener to notifier
+    CAN0_listener = MessageListener()
+    CAN0_notifier = can.Notifier(CAN0, [CAN0_listener])  # Assign listener to notifier
 
     msg_data = [(0xF0), (0xF0), (0xF0), (0xF0)]
     msg = can.Message(arbitration_id = 0x57, data = msg_data, is_extended_id = False)
 
-    CAN1.send(msg)
+    CAN0.send(msg)
     time.sleep(0.001)  # Allow some time for message handling
 
-    CAN1_notifier.stop()
-    CAN1.shutdown()
+    CAN0_notifier.stop()
+    CAN0.shutdown()
 
     os.system("sudo ip link set can0 down")
 
