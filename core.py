021d9605473b9583a1d845736de0fd7cd7bd9843
XanderHayhoe <xanderhayhoe@gmail.com>
2024-05-05 02:37:29 -0400

    pls

--- core.py
+++ core.py
@@ -1,20 +1,4 @@
 import can
 
-import time
-
-with can.Bus() as bus:
-    msg = can.Message(
-        arbitration_id=0xC0FFEE,
-        data=[0, 25, 0, 1, 3, 1, 4, 1],
-        is_extended_id=True
-    )
-    try:
-        bus.send(msg)
-        print(f"Message sent on {bus.channel_info}")
-    except can.CanError:
-        print("Message NOT sent")
-
-
-with can.Bus() as bus:
-    for msg in bus:
-        print(msg.data)
+bus = can.interface.Bus(channel="can0", bustype="socketcan_native")
+notifier = can.Notifier(bus, [can.Printer()])
--- custom_listener.py
+++ custom_listener.py
@@ -1,25 +1,5 @@
 import can
-import time
 
-# Define a listener class that can handle messages
-class SomeListener(can.Listener):
-    def on_message_received(self, msg):
-        print(msg)
-
-# Setup the CAN bus
-my_bus = can.Bus(channel='can0', bustype='socketcan', receive_own_messages=True)
-
-# Instantiate the listener object
-listener = SomeListener()
-
-# Use a Notifier to handle messages asynchronously
-notifier = can.Notifier(my_bus, [listener])
-
-try:
-    # The script now runs and listens for messages indefinitely
-    while True:
-        time.sleep(1)  # Pause the loop to prevent it from running too fast
-except KeyboardInterrupt:
-    # Stop the notifier to clean up resources when you stop the script manually
-    notifier.stop()
-    print("Stopped listening on CAN bus.")
+bus = can.interface.Bus(channel="can0", bustype="socketcan_native")
+msg = can.Message(arbitration_id=0x7de,data=[0, 25, 0, 1, 3, 1, 4, 1])
+bus.send(msg)
