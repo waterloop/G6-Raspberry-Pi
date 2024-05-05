80ee5a4ee9813637825990dfee746c08e280df42
XanderHayhoe <xanderhayhoe@gmail.com>
2024-05-05 02:51:21 -0400

    pls

--- custom_listener.py
+++ custom_listener.py
@@ -4,8 +4,9 @@ bus = can.interface.Bus(channel="can0", bustype="socketcan", receive_own_message
 msg = can.Message(arbitration_id=0x7de, data=[0, 25, 0, 1, 3, 1, 4, 1], is_extended_id=False)
 
 try:
-    bus.send(msg)
-    print("Message sent")
+    while True:
+        bus.send(msg)
+        print("Message sent")
 except can.CanError as e:
     print("Message failed to send", e)
 
